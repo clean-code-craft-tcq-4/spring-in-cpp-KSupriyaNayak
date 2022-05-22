d76c4ee8f9707e6bc94446ecef76255927dedc25
KSupriyaNayak <supriyanayakk@gmail.com>
2022-05-22 23:26:47 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,71 @@
 #include <vector>
+#include <limits>
+using namespace std;
 
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+    class Stats
+    {
+        public:
+            float min,max,avg;
+            Stats()
+        {
+            min = numeric_limits<float>::quiet_NaN();
+            max = numeric_limits<float>::quiet_NaN();
+            avg = numeric_limits<float>::quiet_NaN();
+        }
+    };
+    Stats ComputeStatistics(const vector<float>& vect);
 }
+
+class IAlerter
+{
+    public:
+        virtual void sendAlert() = 0;
+};
+
+class CEmailAlert:public IAlerter
+{
+    public:
+        bool emailSent;
+    CEmailAlert():
+        emailSent(false)
+    {}
+    void sendAlert()
+    {
+        emailSent = true;
+    }
+};
+
+class CLEDALert:public IAlerter
+{
+    public:
+        bool ledGlows;
+    CLEDALert():
+        ledGlows(false)
+    {}
+    void sendAlert()
+    {
+        ledGlows = true;
+    }
+};
+
+class StatsAlerter
+{
+    float maxThreshold;
+    vector<IAlerter*> Alerts;
+    StatsAlerter(const float maxTh, vector<IAlerter*>alts):
+    maxThreshold(maxTh),Alerts(alts)
+    {}
+    
+	void checkAndAlert(const vector<float>& vect)
+	{
+		auto ComputedStats = Statistics::ComputeStatistics(vect);
+		if ( ComputedStats.max > maxThreshold )
+		{
+			for (int i = 0; i < Alerts.size(); ++i)
+			{
+				Alerts[i]->sendAlert();
+			}
+		}
+	}      
+};
