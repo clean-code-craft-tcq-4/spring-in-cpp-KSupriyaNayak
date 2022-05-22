083c97bc909d1a5092475a7a4e178671f4981999
KSupriyaNayak <supriyanayakk@gmail.com>
2022-05-23 00:38:04 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -7,13 +7,13 @@ Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& vect) 
 
 	if (!(vect.empty()))
 	{
-		float maxElement = *std::max_element(vect.begin(), vect.end());
-		float minElement = *std::min_element(vect.begin(), vect.end());
-		float average = accumulate(vect.begin(), vect.end(), 0.0) / vect.size();
+            float maxElement = *std::max_element(vect.begin(), vect.end());
+	    float minElement = *std::min_element(vect.begin(), vect.end());
+	    float average = accumulate(vect.begin(), vect.end(), 0.0) / vect.size();
 
-		st.avg = average;
-		st.max = maxElement;
-		st.min = minElement;
+	    st.avg = average;
+	    st.max = maxElement;
+	    st.min = minElement;
 	}
 	return st;
 }
